4b9317608d4179195127643aaf119f10171f507d
tannineo <tannineo@outlook.com>
2019-11-15 15:57:56 +0000

    try to fix tags [] issue

--- app/service/user.py
+++ app/service/user.py
@@ -48,6 +48,7 @@ def create_user(username, password_not_hashed, email):
     user.username = username
     user.password = hashed_password
     user.email = email
+    user.tags = []
     user.save()
 
     current_app.logger.info('create user with id:' + str(user.id))
