dbcb96dd9bf4da12816cf21dab4c353724ce3277
tannineo <tannineo@outlook.com>
2019-11-20 10:21:18 +0000

    false data first

--- app/controller/picture.py
+++ app/controller/picture.py
@@ -6,100 +6,149 @@ from service.user import recommender
 from service.picture import get_by_tag, upload
 from .auth_decorator import login_required
 
-
-
 picture_api = Namespace(
     'picture',
     description='Picture Controller, search, recommend, like and upload')
+
 
 @picture_api.route('/search')
 class PictureSearch(Resource):
     @picture_api.doc('search')
     @login_required()
-
     def get(self):
         search_str = request.args.get("search")
         if (search_str == ''):
-            return {'msg': '400', 'result': "Please include a tag to search"}
+            raise Exception('empty search string')
 
-        pictures = get_by_tag(search_str)
-        if (pictures == None):
-            return {'msg': '404', 'result': "No pictures found"}
-        else:
-            return {'msg': 'OK', 'result': {'picutres' : pictures.to_json()}}
+        # pictures = get_by_tag(search_str)
+        # return {'msg': 'OK', 'result': {'picutres': pictures.to_json()}}
+
+        return {
+            'msg': 'OK',
+            'result': {
+                'prefix':
+                'http://34.65.122.100:4000/img/',
+                'pictures': [{
+                    'id': 'aaa111',
+                    'name': 'horse-00001',
+                    'url': '/00001-horse.jpg'
+                }, {
+                    'id': 'bbb111',
+                    'name': 'cat-00001',
+                    'url': '/00001-cat.jpg'
+                }, {
+                    'id': 'ccc111',
+                    'name': 'elephant-00001',
+                    'url': '/00001-elephant.jpg'
+                }, {
+                    'id': 'ddd111',
+                    'name': 'dog-00001',
+                    'url': '/00001-dog.jpg'
+                }, {
+                    'id': 'eee111',
+                    'name': 'squirrel-00001',
+                    'url': '/00001-squirrel.jpg'
+                }]
+            }
+        }
 
 
 @picture_api.route('/recommend')
 class PictureRecommend(Resource):
     @picture_api.doc('recommend')
     @login_required()
-
     def get(self):
-        return {'msg': '404', 'result': "No pictures found"}
 
-        user = g.user
-        picture_ids = []
-        picture_ids = recommender(g.user_id)
-        if (picture_ids == []):
-            tag = user.tags[0]
-            pictures = get_by_tag(tag)
+        # user = g.user
+        # picture_ids = []
+        # picture_ids = recommender(g.user_id)
 
-        else:
-            pictures = Picture.objects(_id__in=picture_ids)
-        
-        if (pictures == None):
-            return {'msg': '404', 'result': "No pictures found"}
-        else:
-            return {'msg': 'OK', 'result': pictures.to_json()}
+        # pictures
+        # if (picture_ids == []):
+        #     tag = user.tags[0]
+        #     pictures = get_by_tag(tag)
+
+        # else:
+        #     pictures = Picture.objects(_id__in=picture_ids)
+
+        # return {'msg': 'OK', 'result': pictures.to_json()}
+
+        return {
+            'msg': 'OK',
+            'result': {
+                'prefix':
+                'http://34.65.122.100:4000/img/',
+                'pictures': [{
+                    'id': 'aaa111',
+                    'name': 'horse-00001',
+                    'url': '/00001-horse.jpg'
+                }, {
+                    'id': 'bbb111',
+                    'name': 'cat-00001',
+                    'url': '/00001-cat.jpg'
+                }, {
+                    'id': 'ccc111',
+                    'name': 'elephant-00001',
+                    'url': '/00001-elephant.jpg'
+                }, {
+                    'id': 'ddd111',
+                    'name': 'dog-00001',
+                    'url': '/00001-dog.jpg'
+                }, {
+                    'id': 'eee111',
+                    'name': 'squirrel-00001',
+                    'url': '/00001-squirrel.jpg'
+                }]
+            }
+        }
+
 
 like_fields = picture_api.model(
     'like', {
-        'pic_id': fields.Integer(required=True,description='picture id'),
+        'pic_id': fields.Integer(required=True, description='picture id'),
     })
+
+
 @picture_api.route('/like')
 class PictureLike(Resource):
     @picture_api.doc('like', body=like_fields)
     @login_required()
-
     def post(self):
 
         json = request.get_json()
 
-        """validate req data"""
-        error = ''
-
         if not json['pic_id']:
-            error = 'pic_id is required.'
-        
-        if error is '':
-            pic_id = json['pic_id']
-            picture = Picture.objects(id=pic_id)
+            raise Exception('pic_id is required.')
 
-            user = g.user
-            user.update(push__likes=picture)
-            
+        # pic_id = json['pic_id']
+        # picture = Picture.objects(id=pic_id)
 
-        else:
-            return {'msg': error}, 400
+        # user = g.user
+        # user.update(push__likes=picture)
+
+        return {
+            'msg': 'OK',
+        }
 
 
 upload_fields = picture_api.model(
     'upload', {
-        'name': fields.String(required=True, description='name'),
-        'img_url': fields.String(required=True,description='image url'),
-        'tags': fields.List(cls_or_instance=fields.String,required=True, description='tags'),
+        'name':
+        fields.String(required=True, description='name'),
+        'img_url':
+        fields.String(required=True, description='image url'),
+        'tags':
+        fields.List(
+            cls_or_instance=fields.String, required=True, description='tags'),
     })
-
 
 
 @picture_api.route('/upload')
 class PictureUpload(Resource):
     @picture_api.doc('upload', body=upload_fields)
     @login_required()
-
     def post(self):
         json = request.get_json()
-
         """validate req data"""
         error = None
 
@@ -113,11 +162,10 @@ class PictureUpload(Resource):
         if error is None:
             """service logic"""
             picture = upload(name=json['name'],
-                            image=json['img_url'],
-                            tags=json['tags'])
+                             image=json['img_url'],
+                             tags=json['tags'])
             print(picture.to_json())
             return {'msg': 'OK', 'result': picture.to_json()}
-
 
         else:
             return {'msg': error}, 400
